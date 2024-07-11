f6da3c4e3cad120737e847d58767c75a91b70988
Aerin <rkddofls5@gmail.com>
2024-07-11 15:13:37 +0900

    검색 모달창

--- get.js
+++ get.js
@@ -1,31 +1,37 @@
-
-
+// FHIR 서버의 기본 URL과 리소스 타입
 const base = 'http://hapi.fhir.org/baseR4'; // FHIR 서버의 기본 URL
 const type = 'Patient'; // 요청할 리소스 타입
-const id = '44791747'; // 요청할 리소스의 ID
 
-const url = `${base}/${type}/${id}`;
+// FHIR 서버에서 환자 데이터를 가져오는 함수
+export async function fetchPatientData(patientId, patientName) {
+    let url = `${base}/${type}`;
+    
+    if (patientId) {
+        url += `?_id=${patientId}`;
+    } else if (patientName) {
+        url += `?name=${patientName}`;
+    }
 
-fetch(url, {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/fhir+json', // 원하는 MIME 타입
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/fhir+json', // 원하는 MIME 타입
+            }
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('데이터 가져오기 성공:', data);
+            return data;
+        } else if (response.status === 410) {
+            console.error('리소스가 삭제되었습니다.');
+        } else if (response.status === 404) {
+            console.error('리소스를 찾을 수 없습니다.');
+        } else {
+            console.error('요청 중 오류가 발생했습니다.');
+        }
+    } catch (error) {
+        console.error('네트워크 오류가 발생했습니다:', error);
     }
-})
-.then(response => {
-    if (response.ok) {
-        return response.json();
-    } else if (response.status === 410) {
-        console.error('리소스가 삭제되었습니다.');
-    } else if (response.status === 404) {
-        console.error('리소스를 찾을 수 없습니다.');
-    } else {
-        console.error('요청 중 오류가 발생했습니다.');
-    }
-})
-.then(data => {
-    console.log(data); // 리소스 데이터를 처리
-})
-.catch(error => {
-    console.error('네트워크 오류가 발생했습니다:', error);
-});
+}
